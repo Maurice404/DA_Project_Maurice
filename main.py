3ad3ff2d535ca13d7312ae73056be6ae7cd6785b
Fizzy Bare <bananayolo23@gmail.com>
2021-02-19 01:50:58 +0000

    e

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 #########################################################################
 #Title: PYTHON Project Scenario - Data Analysis
-#Description: This program allows user to analyse.......
-#Name: <...>
-#Group Name: <...>
-#Class: <...>
-#Date: <...>
-#Version: <...>
+#Description: This program allows user to analyse Top 3 Countries that visited Singapore from 2007-2017
+#Name: <Maurice>
+#Group Name: <Name>
+#Class: <PN2004L>
+#Date: <09/02/2021>
+#Version: <1.0>
 #########################################################################
 
 #########################################################################
@@ -38,24 +38,72 @@ class DataAnalysis:
 #########################################################################
 def sortCountry(df):
 
+
+
+
   #print number of rows in dataframe
   print("There are " + str(len(df)) + " data rows read. \n")
 
   #display dataframe (rows and columns)
   print("The following dataframe for Europe from 2007 to 2017 are read as follows: \n")
   print(df)
-
   
 
   #display a specific country (Australia) in column #33
-  country_label = df.columns[20]
-  print("\n\n" + country_label + "was selected.")
+  #country_label = df.columns[20]
+  #print("\n\n" + country_label + "was selected.")
+
+  #print(df[['Year', 'Month', ' United Kingdom ', ' Germany ', ' France ', ' Italy ', ' Netherlands ', ' Greece ', ' Belgium & Luxembourg ', ' Switzerland ', ' Austria ', ' Scandinavia ', ' CIS & Eastern Europe ']])
+
+  Months = df[['Year', 'Month', ' United Kingdom ', ' Germany ', ' France ', ' Italy ', ' Netherlands ', ' Greece ', ' Belgium & Luxembourg ', ' Switzerland ', ' Austria ', ' Scandinavia ', ' CIS & Eastern Europe ']]
+
+  print('The months')
+  print(Months)
+
+
+  Year = Months.iloc[348:479]
+  print('The time period from 2007-2017, Europe region')
+  print(Year)
+  
+
+
+  Selected = Year[[' United Kingdom ', ' Germany ', ' France ', ' Italy ', ' Netherlands ', ' Greece ', ' Belgium & Luxembourg ', ' Switzerland ', ' Austria ', ' Scandinavia ', ' CIS & Eastern Europe ']]
+  print('The selected countries')
+  print(Selected)
+  Selected = Selected.apply(pd.to_numeric)
+
+  #Selected = Year[[' Greece ', ' Austria ', ' Belgium & Luxembourg ']]
+  #Selected = Year[[' Greece ']]
+
+  Total = Selected.sum(axis=0)
+  
+  print(Total)
+
+
+
+  Sorted = Total.sort_values(ascending = False)
+
+  Top3 = Sorted.head(3)
+
+  print("\nTop 3 Countries that visited Singapore")
+  print(Top3)
+
+  print("\nFrom most to least visits")
+  print(Sorted)
+
+  #lmao = input("\nEnter Something:")
+  
+
+  
+
+
+
   
 
   #display a sorted dataframe based on selected country
-  print(" The" + country_label + "was sorted in ascending order. \n")
-  sorted_df =df.sort_values(country_label,ascending=[0])
-  print(sorted_df)
+  #print(" The" + country_label + "was sorted in ascending order. \n")
+  #sorted_df =df.sort_values(country_label,ascending=[0])
+  #print(sorted_df)
 
   return
 #########################################################################
